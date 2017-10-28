cbe0b5f7654d6f890b985a0bfbc0cef070b82a8c
knawara <blueeyedhush@gmail.com>
2017-10-28 11:34:39 +0200

    implemented Workspace

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 
+using ArrSize = size_t;
+using NumType = double;
+
+const ArrSize N = 1000;
+
+class Workspace {
+public:
+	Workspace(const ArrSize size) : size(size) {
+		front = new NumType[size*size];
+		back = new NumType[size*size];
+	}
+
+	~Workspace() {
+		delete[] front;
+		delete[] back;
+	}
+
+	/**
+	 * @return reference to (i,j)th front buffer element
+	 */
+	inline NumType& elf(const ArrSize i, const ArrSize j) {
+		return front[size*i+j];
+	}
+
+	/**
+	 * @return reference to (i,j)th back buffer element
+	 */
+	inline NumType& elb(const ArrSize i, const ArrSize j) {
+		return back[size*i+j];
+	}
+
+	void swap() {
+		NumType* tmp = front;
+		front = back;
+		back = tmp;
+	}
+
+private:
+	const ArrSize size;
+	NumType *front;
+	NumType *back;
+
+};
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Sequential variant" << std::endl;
+
+	Workspace w(N);
+
 	return 0;
 }
