6723674dc165f0a00221f99f3e3809acd2de40cf
knawara <blueeyedhush@gmail.com>
2017-10-29 07:14:03 +0100

    dumping results to file

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 #include <cstddef>
 #include <cmath>
+#include <limits>
+#include <string>
+#include <fstream>
 
 using Coord = size_t;
 using NumType = double;
+const auto NumPrecision = std::numeric_limits<NumType>::max_digits10;
 
 /* dimension of inner work area (without border) */
 const Coord N = 1000;
@@ -14,7 +18,9 @@ const Coord N = 1000;
  */
 class Workspace {
 public:
-	Workspace(const Coord size) : innerSize(size), actualSize( (innerSize+2)*(innerSize+2) ) {
+	Workspace(const Coord size)
+			: innerLength(size), outerLength(size+2), actualSize( outerLength*outerLength )
+	{
 		front = new NumType[actualSize];
 		back = new NumType[actualSize];
 	}
@@ -28,14 +34,14 @@ public:
 	 * @return reference to (i,j)th front buffer element
 	 */
 	inline NumType& elf(const Coord x, const Coord y) {
-		return front[actualSize*x+y];
+		return front[outerLength*x+y];
 	}
 
 	/**
 	 * @return reference to (i,j)th back buffer element
 	 */
 	inline NumType& elb(const Coord x, const Coord y) {
-		return back[actualSize*x+y];
+		return back[outerLength*x+y];
 	}
 
 	void zeroBuffers(const NumType b) {
@@ -45,6 +51,8 @@ public:
 		}
 	}
 
+	Coord getEdgeLength() {return innerLength+2;}
+
 	void swap() {
 		NumType* tmp = front;
 		front = back;
@@ -52,11 +60,35 @@ public:
 	}
 
 private:
-	const Coord innerSize;
+	const Coord innerLength;
+	const Coord outerLength;
 	const Coord actualSize;
 	NumType *front;
 	NumType *back;
 
+};
+
+class FileDumper {
+public:
+	FileDumper(const std::string targetFile) {
+		file.open(targetFile);
+		file.precision(NumPrecision);
+	}
+
+	~FileDumper() {
+		file.close();
+	}
+
+	void dumpBackbuffer(Workspace& w, const NumType t) {
+		for(size_t i = 0; i < w.getEdgeLength(); i++) {
+			for(size_t j = 0; j < w.getEdgeLength(); j++) {
+				file << i << " " << j << " " << t << " " << w.elb(i,j) << std::endl;
+			}
+		}
+	}
+
+private:
+	std::ofstream file;
 };
 
 /*
@@ -72,14 +104,19 @@ int main() {
 	std::cout << "Sequential variant" << std::endl;
 
 	Workspace w(N);
+	FileDumper d("./results");
 
 	/* fill in boundary condition */
 	w.zeroBuffers(0.0);
 	for(size_t x = 0; x < N; x++) {
 		for(size_t y = 0; y < N; y++) {
-			w.elf(x,y) = f(x,y);
+			w.elf(I(x),I(y)) = f(x,y);
 		}
 	}
+
+	w.swap();
+
+	d.dumpBackbuffer(w, 0.0);
 
 	return 0;
 }
