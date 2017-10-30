c8866c58a232c0a4dd7f778ec91e1bd8874c5b84
knawara <blueeyedhush@gmail.com>
2017-10-30 13:04:50 +0100

    introducing Partitiner, where all logic of offset calculation is stored; rewrite seq to use it

--- src/seq.cpp
+++ src/seq.cpp
@@ -5,12 +5,19 @@
 #include "shared.h"
 
 /**
- * Work area is indexed from 1 (first element) to size (last element)
+ * Work area is indexed from 0 to size-1
+ * Around that we have border area, which can be accessed using -1 and size indices
+ *
+ * When plotting results, border counts as 0, but plotter doesn't plot border values
+ * (easier to implement that way, same plotter can be used for seq and parallel)
  */
 class Workspace {
 public:
-	Workspace(const Coord size)
-			: innerLength(size), outerLength(size+2), actualSize( outerLength*outerLength )
+	Workspace(const Coord size) :
+			innerLength(size),
+			outerLength(size+2),
+			actualSize(outerLength*outerLength),
+			zeroOffset(1)
 	{
 		front = new NumType[actualSize];
 		back = new NumType[actualSize];
@@ -25,14 +32,14 @@ public:
 	 * @return reference to (i,j)th front buffer element
 	 */
 	inline NumType& elf(const Coord x, const Coord y) {
-		return front[outerLength*x+y];
+		return front[coords(x,y)];
 	}
 
 	/**
 	 * @return reference to (i,j)th back buffer element
 	 */
 	inline NumType& elb(const Coord x, const Coord y) {
-		return back[outerLength*x+y];
+		return back[coords(x,y)];
 	}
 
 	void zeroBuffers(const NumType b) {
@@ -42,7 +49,7 @@ public:
 		}
 	}
 
-	Coord getEdgeLength() {return outerLength;}
+	Coord getInnerLength() {return innerLength;}
 
 	void swap() {
 		NumType* tmp = front;
@@ -51,41 +58,46 @@ public:
 	}
 
 private:
+	const Coord zeroOffset;
 	const Coord innerLength;
 	const Coord outerLength;
 	const Coord actualSize;
 	NumType *front;
 	NumType *back;
 
+	inline Coord coords(const Coord x, const Coord y) {
+		return (zeroOffset+outerLength)*x+(y+zeroOffset);
+	}
 };
 
 
 int main(int argc, char **argv) {
 	auto conf = parse_cli(argc, argv);
 
-	Timer timer;
-	Workspace w(conf.N);
-	FileDumper<Workspace> d("./results/t", conf.N, 0.0, 0.0, 1.0/conf.N, 1);
-
-	#define I(IDX) IDX+1
-	#define V(IDX) (IDX+1)*1.0/conf.N
+	Partitioner p(1, 0.0, 1.0, conf.N);
 
 	/* calculate helper values */
 	const TimeStepCount dumpEvery = conf.timeSteps/DUMP_TEMPORAL_FREQUENCY;
+	const NumType h = p.get_h();
+	const Coord n = p.partition_inner_size();
+
+	Timer timer;
+	Workspace w(conf.N);
+	NumType x_off, y_off;
+	std::tie(x_off, y_off) = p.get_math_offset(0,0);
+	FileDumper<Workspace> d("./results/t", n, x_off, y_off, h);
 
 	timer.start();
 	/* fill in boundary condition */
-	for(Coord x_idx = 0; x_idx < conf.N; x_idx++) {
-		for(Coord y_idx = 0; y_idx < conf.N; y_idx++) {
-	 		auto x_i = I(x_idx);
-			auto y_i = I(y_idx);
-			auto x = V(x_idx);
-			auto y = V(y_idx);
+	for(Coord x_idx = 0; x_idx < n; x_idx++) {
+		for(Coord y_idx = 0; y_idx < n; y_idx++) {
+			auto x = x_idx*h;
+			auto y = y_idx*h;
 			auto val = f(x,y);
-			w.elf(x_i,y_i) = val;
+			w.elf(x_idx, y_idx) = val;
 
 			#ifdef DEBUG
-			std::cerr << "[" << x_i-1 << "," << y_i-1 <<"] "
+			std::cerr << "[" << x_idx << "," << y_idx <<"] "
 			          << "(" << x << "," << y << ") -> "
 			          << val << std::endl;
 			#endif
@@ -94,15 +106,14 @@ int main(int argc, char **argv) {
 
 	w.swap();
 
-
 	for(TimeStepCount step = 0; step < conf.timeSteps; step++) {
-		for(Coord x_idx = 0; x_idx < conf.N; x_idx++) {
-			for(Coord y_idx = 0; y_idx < conf.N; y_idx++) {
-				w.elf(I(x_idx), I(y_idx)) = equation(
-					 w.elb(I(x_idx-1), I(y_idx)),
-					 w.elb(I(x_idx), I(y_idx-1)),
-					 w.elb(I(x_idx+1), I(y_idx)),
-					 w.elb(I(x_idx), I(y_idx+1))
+		for(Coord x_idx = 0; x_idx < n; x_idx++) {
+			for(Coord y_idx = 0; y_idx < n; y_idx++) {
+				w.elf(x_idx, y_idx) = equation(
+					 w.elb(x_idx-1 ,y_idx),
+					 w.elb(x_idx ,y_idx-1),
+					 w.elb(x_idx+1 ,y_idx),
+					 w.elb(x_idx ,y_idx+1)
 				);
 			}
 		}
--- src/shared.h
+++ src/shared.h
@@ -17,7 +17,6 @@
 
 // #define DEBUG
 
-// @todo modified while working on parallel, possible source of bugs
 using Coord = long long;
 using TimeStepCount = size_t;
 using NumType = double;
@@ -28,6 +27,116 @@ using Duration = long long;
 /* dimension of inner work area (without border) */
 const Coord DUMP_SPATIAL_FREQUENCY = 25;
 const TimeStepCount DUMP_TEMPORAL_FREQUENCY = 100;
+
+/* 0                       1
+ *    _*_*_*_*_ _*_*_*_*_
+ * * | * * * * | * * * * | *
+ * * | * * * * | * * * * | *
+ *
+ * That's how we partition vertices across nodes; external vertices are boundary conditions
+ * - innerSize - grid length without boundary conditions
+ * - outerSize - grid length including boundary conditions
+ * Non-edge nodes instead of boundary conditions have values copied over from other nodes.
+ *
+ * nodes in each partition are indexed from 0 to n_partitioned-1
+ * global offset of nodes in partition is (0, n_partitioned)
+ * but if we want math offset, we get (0, (n_partitioned+1)*h) /because of boundary being outside/
+ *
+ * WRONG!
+ * if points in-between, then we have h/2 from the boundary
+ *   it shouldn't be a problem, since boundary is 0 everywhere? (but outside, not inside)
+ * additionally, if we divide into 40 intervals, we have 39 (not 38 points) in between
+ * if we have 0 and 1 aligned with points, we don't have such problems
+ *
+ * N = 4
+ * outer = 6
+ * on [0,1] interval
+ * divided by 6 we get 1/6th...
+ * if we have 4 points in the middle, we should divide by 5 (6 points == 5 intervals)!!!
+ *
+ */
+class Partitioner {
+	/* Responsibilities
+	 * - check for partitioning correctness
+	 * - get index and numerical offsets
+	 */
+public:
+	Partitioner(const Coord node_count, const NumType lower_b, NumType upper_b, const Coord grid_dimm)
+			: nodeCount(node_count), lowerB(lower_b), upperB(upper_b), grid_dimm(grid_dimm)
+	{
+		h = (upperB - lowerB)/(grid_dimm+1);
+		verify_values();
+	}
+
+	Coord get_node_side_len() {
+		return sideLen;
+	}
+
+	Coord partition_inner_size() {
+		return grid_dimm/sideLen;
+	}
+
+	NumType get_h() {
+		return h;
+	}
+
+	/*
+	 * Indexing offsets across machine grid
+	 */
+	std::pair<Coord, Coord> get_indexing_offset(const int row, const int column) {
+		return std::make_pair(row*mh, column*mh);
+	};
+
+	std::pair<Coord, Coord> get_indexing_offset(const int nodeId) {
+		int row, column;
+		std::tie(row, column) = node_id_to_grid_pos(nodeId);
+		return get_indexing_offset(row, column);
+	};
+
+	/**
+	 * Math offsets across point grid
+	 */
+	std::pair<NumType, NumType> get_math_offset(const int i, const int j) {
+		return std::make_pair((i+1)*h, (j+1)*h);
+	};
+
+private:
+	/* characteristics of node grid */
+	const int nodeCount;
+	int sideLen;
+	Coord row;
+	Coord column;
+
+	/* qualities linking both worlds */
+	int mh;
+
+	/* characteristics of stored values and point grids */
+	const NumType lowerB;
+	const NumType upperB;
+	const Coord grid_dimm;
+	NumType h;
+
+	void verify_values() {
+		auto sqr = static_cast<long>(std::sqrt(nodeCount));
+		if(sqr*sqr != nodeCount) {
+			throw std::runtime_error("numer of nodes must be square");
+		} else {
+			sideLen = sqr;
+		}
+
+		if(grid_dimm % sideLen != 0) {
+			throw std::runtime_error("point grid len must be evenly divisible by machine grid len");
+		} else {
+			mh = grid_dimm/sideLen;
+		}
+	}
+
+	std::pair<int, int> node_id_to_grid_pos(int nodeId) {
+		const auto row = nodeId/sideLen;
+		const auto column = nodeId%sideLen;
+		return std::make_pair(row, column);
+	};
+};
 
 /* for nice plot: N = 40, timeSteps = 400 */
 struct Config {
@@ -64,6 +173,9 @@ Config parse_cli(int argc, char **argv) {
 	return conf;
 }
 
+/**
+ * It doesn't plot borders, so it always queries workspace from 0 to size-1
+ */
 template <typename W>
 class FileDumper {
 public:
@@ -71,17 +183,16 @@ public:
 	           const Coord n_partition,
 	           const NumType offset_x,
 	           const NumType offset_y,
-	           const NumType step,
-	           const Coord off)
-			: prefix(prefix), N(n_partition), offset_x(offset_x), offset_y(offset_y), step(step), cordOff(off) {}
+	           const NumType step)
+			: prefix(prefix), N(n_partition), offset_x(offset_x), offset_y(offset_y), step(step) {}
 
-	void dumpBackbuffer(W& w, const Coord t, const Coord linearDensity = DUMP_SPATIAL_FREQUENCY) {
+	void dumpBackbuffer(W& w, const Coord t, const Coord keep_snapshots = DUMP_SPATIAL_FREQUENCY) {
 		#ifdef DEBUG
 		std::cerr << "offset_x: " << offset_x << ", offset_y: " << offset_y << std::endl;
 		#endif
 
-		auto edgeLen  = w.getEdgeLength();
-		auto step = edgeLen/linearDensity;
+		auto edgeLen  = w.getInnerLength();
+		auto step = edgeLen/keep_snapshots;
 
 		assert(step > 0);
 
@@ -105,7 +216,7 @@ public:
 			loop(edgeLen, step, [=, &w, &file](const Coord j) {
 				auto x = vr_x(i);
 				auto y = vr_y(j);
-				file << x << " " << y << " " << t << " " << w.elb(i+cordOff,j+cordOff) << std::endl;
+				file << x << " " << y << " " << t << " " << w.elb(i,j) << std::endl;
 			});
 
 			file << std::endl;
@@ -124,7 +235,6 @@ private:
 	const Coord N;
 	std::ostringstream filename;
 
-	const Coord cordOff;
 	const NumType offset_x;
 	const NumType offset_y;
 	const NumType step;
@@ -135,8 +245,14 @@ private:
 
 		while(iShouldContinue) {
 			if(i >= limit) {
-				i = limit-1;
 				iShouldContinue = false;
+
+				/* should we do one more iteration with variable exact to limit-1? */
+				if(i - limit > step/4) {
+					i = limit-1;
+				} else {
+					break;
+				}
 			}
 
 			f(i);
