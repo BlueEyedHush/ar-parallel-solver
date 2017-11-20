ca33f911599a350bfab9aea7433f32c6651e6b74
knawara <blueeyedhush@gmail.com>
2017-11-20 12:12:56 +0100

    adding bench/test mode + nobuild switch

--- superc/pth/bootstrapper.sh
+++ superc/pth/bootstrapper.sh
@@ -3,35 +3,62 @@
 SCRIPT_DIR="$(readlink -e $(dirname ${BASH_SOURCE[0]}))"
 BASE_DIR="$(readlink -e $(dirname ${BASH_SOURCE[0]})/../../)"
 
+USAGE="bootstrapper.sh <variant> <node num> test|bench [nobuild]"
+
 if [ -z "$1" ]; then
-    echo "Must specify executable to run"
+    echo "$USAGE"
     exit 1
 fi
 
-TPN="$2"
 if [ -z "$2" ]; then
-    TPN=4
+    echo "$USAGE"
+    exit 1
+else
+    TPN="$2"
 fi
 
+if [ -z "$3" ]; then
+    echo "$USAGE"
+else
+    MODE="$3"
+fi
 
-"$SCRIPT_DIR"/build.sh "$1"
+if [ -z "$4" ]; then
+    "$SCRIPT_DIR"/build.sh "$1"
+fi
 
 rm -f "$BASE_DIR"/ar.se
 rm -f "$BASE_DIR"/ar.so
 
-CMD="sbatch
-    -J ar-1
-    -N 1
-    --ntasks-per-node $TPN
-    --mem 1gb
-    --time=00:10:00
-    -A ccbmc6
-    -p plgrid-testing
-    --output ar.so
-    --error ar.se
-    --mail-type=END,FAIL
-    --mail-user=knawara112@gmail.com
-    $SCRIPT_DIR/script.sh $1"
+if [ "$MODE" == 'test' ]; then
+    CMD="sbatch
+        -J ar-1
+        -N 1
+        --ntasks-per-node $TPN
+        --mem 1gb
+        --time=00:10:00
+        -A ccbmc6
+        -p plgrid-testing
+        --output ar.so
+        --error ar.se
+        --mail-type=END,FAIL
+        --mail-user=knawara112@gmail.com
+        $SCRIPT_DIR/script.sh $1"
+else
+    CMD="sbatch
+        -J ar-1
+        -N $TPN
+        --ntasks-per-node 1
+        --mem 1gb
+        --time=00:10:00
+        -A ccbmc6
+        -p plgrid-testing
+        --output ar.so
+        --error ar.se
+        --mail-type=END,FAIL
+        --mail-user=knawara112@gmail.com
+        $SCRIPT_DIR/script.sh $1"
+fi
 
 echo "$CMD"
 $CMD
